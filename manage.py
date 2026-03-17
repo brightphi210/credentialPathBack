a622e021eefb3530988479237a270e4c266afb40
brightphi210 <chibuzorphilip2001@gmail.com>
2026-03-17 10:40:42 +0100

    Done with Deployment to MYSQL

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python
+
+import pymysql
+pymysql.install_as_MySQLdb()
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
 
 def main():
     """Run administrative tasks."""
